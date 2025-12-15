ec7d5c1443ee962db90f9ca73cf888e419b61565
Jonghwa Park <suakii@gmail.com>
2025-12-15 16:34:14 +0900

    route

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,5 @@
+<h1>Hello View</h1>
+<p>Route → View 연습</p>
+
+<h1>{{ $school }}</h1>
+<h1>{{ $year }} Laravel Study</h1>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/hello', function(){
+    return "Hello Laravel 11";
+});
+
+Route::get('/hello-view', function(){
+    return view('hello');
+});
+
+
+Route::get('/hello-data', function(){
+    return view('hello', [
+        'school' => '경기과학고',
+        'year' => 2025,
+    ]);
+});
+
+Route::get('/students/{id}', function ($id) {
+    return "학생 ID: {$id}";
+})->whereNumber('id');
